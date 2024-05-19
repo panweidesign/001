f1db9a66a8ab8c4f9ca32f32d691766c5769f6dd
PANWEI <44900640+panweidesign@users.noreply.github.com>
2024-05-19 10:22:09 +0800

    Update and rename 03 to 03.js

---
+++ 03.js
@@ -0,0 +1,41 @@
+/******************************
+
+[rewrite_local]
+
+^https:\/\/api\.gotokeep\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/keep.js
+
+[mitm] 
+
+hostname = api.gotokeep.com
+
+*******************************/
+
+
+var _0x3bb7=['Gm17w7LDqwfCo8K6YsOYwonDqTc7bhk=','LUnDg8KuTMOCJw==','w4LCocOqwrJqHi3Dtl0BwqXDuVg=','fMKqREg=','ZMKkYBZyahA=','w6PDj3Ulwp95wow=','PMKuwrjDtj3CkcKbZ8KqRGLDsXtW','wrPDjmo+wpBWwobDlEsqwqvCuCYtacKQw5nDjg==','NcKQIcKPwoNpcTXCsULCtcOm5byl5Lmg5puj6KWR6ZWYUg==','QWIxQlXClmM=','wp3Cr8K4wqsPwrwJW8KwL8OFw6glRsKbaQLCgA==','Rxp1RcK3HDzDsG5iKMOGw7Zaw6DDgA==','XMKUN3w/w5ZlV1LCgsObZ8OGw4U=','JcKsw7LCslt0wpE=','wqLChAoU','wqwIaMOrZMKuw5A=','w7nDqwzDocOBw7dm','M8K/Z8Kjw4nCsWoLDGzCkMKsDMKJ5beW5rOb5LuKwr0=','wrLCjh4BwoHDvBw=','ZQvDssKFMsKCVg=='];(function(_0x1d3294,_0x3bb7f9){var _0x224c61=function(_0x81749a){while(--_0x81749a){_0x1d3294['push'](_0x1d3294['shift']());}};_0x224c61(++_0x3bb7f9);}(_0x3bb7,0xde));var _0x224c=function(_0x1d3294,_0x3bb7f9){_0x1d3294=_0x1d3294-0x0;var _0x224c61=_0x3bb7[_0x1d3294];if(_0x224c['JEvqhr']===undefined){(function(){var _0x4b1eb;try{var _0x5a5081=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');');_0x4b1eb=_0x5a5081();}catch(_0x2ea530){_0x4b1eb=window;}var _0x3f31e8='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x4b1eb['atob']||(_0x4b1eb['atob']=function(_0x568a1d){var _0x2a292e=String(_0x568a1d)['replace'](/=+$/,'');var _0xf29f4b='';for(var _0x103039=0x0,_0x22dde6,_0x4b5852,_0x5f4706=0x0;_0x4b5852=_0x2a292e['charAt'](_0x5f4706++);~_0x4b5852&&(_0x22dde6=_0x103039%0x4?_0x22dde6*0x40+_0x4b5852:_0x4b5852,_0x103039++%0x4)?_0xf29f4b+=String['fromCharCode'](0xff&_0x22dde6>>(-0x2*_0x103039&0x6)):0x0){_0x4b5852=_0x3f31e8['indexOf'](_0x4b5852);}return _0xf29f4b;});}());var _0x20a7f1=function(_0x415e80,_0x15b263){var _0x49c5bf=[],_0x1dc1d6=0x0,_0x385c72,_0x39abab='',_0x448bd7='';_0x415e80=atob(_0x415e80);for(var _0x4075bf=0x0,_0x259bcb=_0x415e80['length'];_0x4075bf<_0x259bcb;_0x4075bf++)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
